6d32638733b36e240e105d307abb35605c6ddc2c
Diego Mateo Moran <dmoranmateo13@gmail.com>
2024-09-24 20:27:51 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,43 +1,64 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     // Cambio el tipo de variable de los operadores para que lea caracteres
     char opcion;
     float num1, num2, resultado;
     // Modifico la interfaz para que el usuario entienda qué tiene que hacer
-    printf("Ingrese el símbolo de la operación: \n"); 
+    printf("Ingrese el símbolo de la operación: \n");
     printf("Suma: (+)\n");
     printf("Resta: (-)\n");
     printf("Multiplicación: (*)\n");
     printf("División: (/)\n");
+    printf("Comparacion: (C)\n");
     scanf("%c", &opcion);
 
     printf("Ingresa dos números: ");
     scanf("%f %f", &num1, &num2);
     // Modifico el switch para que funcione con los caracteres correspondientes
-    switch(opcion) {
-        case '+':
-            resultado = num1 + num2;
+    switch (opcion)
+    {
+    case '+':
+        resultado = num1 + num2;
+        printf("Resultado: %.2f\n", resultado);
+        break;
+    case '-':
+        resultado = num1 - num2;
+        printf("Resultado: %.2f\n", resultado);
+        break;
+    case '*':
+        resultado = num1 * num2;
+        printf("Resultado: %.2f\n", resultado);
+        break;
+    case '/':
+        if (num2 != 0)
+        {
+            resultado = num1 / num2;
             printf("Resultado: %.2f\n", resultado);
-            break;
-        case '-':
-            resultado = num1 - num2;
-            printf("Resultado: %.2f\n", resultado);
-            break;
-        case '*':
-            resultado = num1 * num2;
-            printf("Resultado: %.2f\n", resultado);
-            break;
-        case '/':
-            if(num2 != 0) {
-                resultado = num1 / num2;
-                printf("Resultado: %.2f\n", resultado);
-            } else {
-                printf("Error: División por cero.\n");
-            }
-            break;
-        default:
-            printf("Opción inválida.\n");
+        }
+        else
+        {
+            printf("Error: División por cero.\n");
+        }
+        break;
+    case 'c':
+    case 'C':
+        if (num1 > num2)
+        {
+            printf("%.2f es mayor que %.2f\n", num1, num2);
+        }
+        else if (num2 > num1)
+        {
+            printf("%.2f es mayor que %.2f\n", num2, num1);
+        }
+        else
+        {
+            printf("Los números son iguales\n");
+        }
+        break;
+    default:
+        printf("Opción inválida.\n");
     }
 
     return 0;
